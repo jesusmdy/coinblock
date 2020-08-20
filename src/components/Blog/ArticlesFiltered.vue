6c4e8540c22a06c57bf7ab90973545ba6a1dece9
Jesús Ramírez <jesusmarcanody@gmail.com>
2020-08-20 15:29:47 -0400

    Some edit

---
+++ src/components/Blog/ArticlesFiltered.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<ArticlesList :articles="category.posts" />
+	</div>
+</template>
+<script>
+export default {
+	name: 'ArticlesFiltered',
+	props: {
+		category: Object
+	},
+	components: {
+		ArticlesList: () => import(`../ArticlesList`)
+	}
+}
+</script>
---
+++ src/containers/assertion/404.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="err">
+		<h2>
+			Whoops, this is an <b>404</b> mistake
+		</h2>
+	</div>
+</template>
+<style>
+.err {
+	height: 500px;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
